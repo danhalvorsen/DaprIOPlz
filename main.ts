36881595708f777cda628fc28d7068074d474df6
Dan Edvard Halvorsen <danhalvorsen@users.noreply.github.com>
2023-03-16 18:20:12 +0100

    add a web app

---
+++ main.ts
@@ -0,0 +1,11 @@
+import {
+  provideFASTDesignSystem,
+  fastCard,
+  fastButton
+} from '@microsoft/fast-components';
+
+provideFASTDesignSystem()
+  .register(
+    fastCard(),
+    fastButton()
+  );
